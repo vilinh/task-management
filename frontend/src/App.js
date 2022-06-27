23aa9b3000f7fd419e17677da02d64698b6057a5
vilinh <vilinhlvu@gmail.com>
2022-06-27 14:52:24 -0700

    edit: sidebar menu & animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Home } from './pages/Home/Home';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 function App() {
   return (
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,45 @@
-import './sidebar.css'
+import "./sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faComment,
+  faGear,
+  faInbox,
+  faLayerGroup,
+} from "@fortawesome/free-solid-svg-icons";
+
 export const Sidebar = () => {
   return (
     <div className="sideBar">
-        <h3>First Last</h3>
+      <div className="profile">
+        <img
+          src="https://cdn.photographycourse.net/wp-content/uploads/2022/04/how-to-take-professional-headshots.jpg"
+          alt="headshot"
+          className="headshot"
+        />
+        <h3 className="name">Anne Cathy</h3>
+        <h4 className="title">Sr Designer</h4>
+      </div>
+      <div className="menu">
+        <span className="menu-item">
+          <FontAwesomeIcon className="icon" icon={faInbox} />
+          Dashboard
+        </span>
+        <span className="menu-item">
+          <FontAwesomeIcon className="icon" icon={faComment} />
+          Inbox
+        </span>
+        <span className="menu-item">
+          <FontAwesomeIcon className="icon" icon={faLayerGroup} /> Projects
+        </span>
+        <span className="menu-item">
+          <FontAwesomeIcon className="icon" icon={faGear} /> Settings
+        </span>
+        <span className="menu-item">
+          <FontAwesomeIcon className="icon" icon={faArrowRightFromBracket} />
+          Logout
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
