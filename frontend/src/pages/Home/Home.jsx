39939a5ff00407aa1b7637605248eeab8e55ab8a
vilinh <vilinhlvu@gmail.com>
2022-06-27 21:17:02 -0700

    home: panels

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,11 +8,27 @@ export const Home = () => {
       <div className="dashBoard">
         <div className="dashboard-left">
           <h1>Dashboard</h1>
-          <h2>Brief Look</h2>
-          <div className="tabs">
-            <button className="tab-btn">Weekly</button>
-            <button className="tab-btn">Monthly</button>
-            <button className="tab-btn">Last year</button>
+          <div className="panels">
+            <div className="panel-top">
+              <h2>Brief Look</h2>
+              <div className="tabs">
+                <button className="tab-btn">Weekly</button>
+                <button className="tab-btn">Monthly</button>
+                <button className="tab-btn">Last year</button>
+              </div>
+              <div className="activity">hello</div>
+            </div>
+
+            <div className="panel-bottom">
+              <h2>Most Popular Projects</h2>
+              <div className="tabs">
+                <button className="tab-btn">Last 30 Days</button>
+                <button className="tab-btn">Last 90 Days</button>
+                <button className="tab-btn">Last year</button>
+                <button className="tab-btn">All Time</button>
+              </div>
+              <div className="activity">hello</div>
+            </div>
           </div>
         </div>
         <div className="dashboard-right">
