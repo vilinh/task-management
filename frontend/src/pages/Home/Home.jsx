829f3e9fb373c432e3f531ebc4cde74c606ca670
vilinh <vilinhlvu@gmail.com>
2022-06-29 07:53:42 -0700

    sidemenu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,11 +3,16 @@ import { Sidebar } from "../../components/Sidebar/Sidebar";
 import { Panel } from "../../components/Panel/Panel";
 import { TodoPanel } from "../../components/TodoPanel/TodoPanel";
 import { Footer } from "../../components/footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faCaretRight, faHamburger } from "@fortawesome/free-solid-svg-icons";
 
 export const Home = () => {
   return (
     <div className="homeContainer">
       <div className="homeWrapper">
+        <div className="navBar">
+          <FontAwesomeIcon className="sideMenu" icon={faBars} />
+        </div>
         <Sidebar className="sideBar" />
         <div className="dashBoard">
           <div className="dashboard-left">
